d46098a78ed108fe88d27ecfc2ae40053a6b469b
root <root@localhost.localdomain>
2019-02-03 11:45:24 -0500

    Добавлены примеры форматов. Исправлена утечка памяти.

--- excelport/src/xlnt/xlntparser.cpp
+++ excelport/src/xlnt/xlntparser.cpp
@@ -180,13 +180,18 @@ QList<Group*> XlntParser::parse(const QByteArray &ba, bool *isOk, QString *errMs
 
 			if (rows.size() == 0)
 			{
-				parserErrMessage.append("Error excel format");
+				parserErrMessage.append("Rows size is empty");
 				parserOk = false;
 			}
 
 			if (parserOk == true)
 			{
 				break;
+			}
+			else
+			{
+				qDeleteAll(res);
+				res.clear();
 			}
 		}
 	}
@@ -195,11 +200,11 @@ QList<Group*> XlntParser::parse(const QByteArray &ba, bool *isOk, QString *errMs
 		*isOk = false;
 		errMsg->append(QString(e.what()));
 	}
-//	catch(...)
-//	{
-//		*isOk = false;
-//		errMsg->append("Unknown error");
-//	}
+	catch(...)
+	{
+		*isOk = false;
+		errMsg->append("Unknown error");
+	}
 
 	if (*isOk == false)
 	{
@@ -208,155 +213,3 @@ QList<Group*> XlntParser::parse(const QByteArray &ba, bool *isOk, QString *errMs
 
 	return res;
 }
-
-//std::vector<xlnt::range_reference> thinOut(const std::vector<xlnt::range_reference> &vector)
-//{
-//	QSet<int> rowProc;
-
-//	std::vector<xlnt::range_reference> res;
-
-//	for (uint i = 0; i < vector.size(); i++)
-//	{
-//		xlnt::range_reference r = vector.at(i);
-//		int bot = r.bottom_left().row();
-//		int top = r.top_right().row();
-
-//		int right = r.bottom_right().column().index;
-//		int left = r.bottom_left().column().index;
-
-//		int prevRight = -1;;
-//		int prevLeft = -1;
-
-//		if (!res.empty())
-//		{
-//			prevRight = res.back().bottom_right().column().index;
-//			prevLeft = res.back().bottom_left().column().index;
-//		}
-
-//		if (top != bot)
-//		{
-//			res.clear();
-//			for (int i = top; i <= bot; i++)
-//			{
-//				rowProc.insert(i);
-//			}
-//		}
-//		else if (rowProc.contains(top))
-//		{
-//			res.clear();
-//		}
-//		else if ( ( (prevLeft == 1 && prevRight == 7)
-//					|| (prevLeft == 5 && prevRight == 7) )
-//				 && (left == 1 && right == 4) )
-//		{
-//			res.push_back(r);
-//		}
-//		else if (prevLeft == 1 && prevRight == 4 && left == 5 && right == 7)
-//		{
-//			res.push_back(r);
-//		}
-//		else if (left == 1 && right == 7)
-//		{
-//			res.push_back(r);
-//		}
-//		else
-//		{
-//			res.clear();
-//		}
-//	}
-
-//	return res;
-//}
-
-//QList<Group *> XlntParser::parse(const QByteArray &ba, bool *isOk, QString *errMsg) const
-//{
-//	QList<Group*> res;
-//	try
-//	{
-//		std::istringstream in(std::string(ba.data(), ba.size()));
-//		xlnt::workbook excelFile;
-//		excelFile.load(in);
-
-//		const xlnt::worksheet ws = excelFile.sheet_by_index(0);
-
-//		std::vector<xlnt::range_reference> range = thinOut(ws.merged_ranges());
-
-//		Group* cfirm = NULL;;
-//		Group* cSub = NULL;
-
-//		Q_FOREACH (xlnt::range_reference r, range)
-//		{
-//			xlnt::cell cell = ws.cell(r.bottom_left().column(), r.bottom_left().row());
-//			bool isSubvision = (cell.font().bold()) && (r.width() == 7);
-//			if (isSubvision == true)
-//			{
-//				if (cSub != NULL)
-//				{
-//					cfirm->addGroup(cSub);
-//				}
-//				cSub = new Group();
-//				cSub->setName(QString(cell.value<std::string>().c_str()));
-//			}
-//			else if (r.width() == 7)
-//			{
-//				if (cfirm != NULL)
-//				{
-//					res.append(cfirm);
-//				}
-//				cfirm = new Group();
-//				cfirm->setName(QString(cell.value<std::string>().c_str()));
-//			}
-//			else if (r.width() == 4)
-//			{
-//				QString vacancy = QString(ws.cell(xlnt::column_t(r.bottom_left().column().index + 4),
-//												  r.bottom_left().row())
-//										  .value<std::string>().c_str());
-
-//				Group* v = cSub->findGroup(vacancy);
-//				if (v == NULL)
-//				{
-//					v = new Group;
-//					v->setName(vacancy);
-//					cSub->addGroup(v);
-//				}
-
-//				QStringList fio = QString(cell.value<std::string>().c_str()).split(" ");
-//				if (fio.size() >= 2)
-//				{
-//					People* p = new People;
-//					p->setName(fio.value(1));
-//					p->setSurname(fio.value(0));
-//					p->setPatronymic(fio.value(2));
-//					v->addPeople(p);
-//				}
-//			}
-//		}
-
-//		if (cSub != NULL)
-//		{
-//			cfirm->addGroup(cSub);
-//		}
-
-//		if (cfirm != NULL)
-//		{
-//			res.append(cfirm);
-//		}
-//	}
-//	catch(xlnt::exception &e)
-//	{
-//		*isOk = false;
-//		errMsg->append(QString(e.what()));
-//	}
-//	catch(...)
-//	{
-//		*isOk = false;
-//		errMsg->append("Unknown error");
-//	}
-
-//	if (*isOk == false)
-//	{
-//		qCritical() << Q_FUNC_INFO << ":" << *errMsg;
-//	}
-
-//	return res;
-//}
